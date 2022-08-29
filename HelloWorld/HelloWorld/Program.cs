926a24694496241011ba862e495fc94a0dc751d5
IsabelleFSNunes <bellefsnunes@gmail.com>
2022-08-29 20:24:13 -0300

    Adicionando mais algumas alterações -- tipos numericos -- Isabelle

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       // Outras alterações realizadas por isabelle.
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("Integer Types (signed):");
+        sbyte sb = 20;
+        short s = -32;
+        int i = -1;
+        long l = -3000;
+
+        Console.WriteLine("sbyte -------\t" + sb + "\t sizeof = " + sizeof(sbyte) + " Bytes");
+        Console.WriteLine("short -------\t" + s + "\t sizeof = " + sizeof(short) + " Bytes");
+        Console.WriteLine("int   -------\t" + i + "\t sizeof = " + sizeof(int) + " Bytes");
+        Console.WriteLine("long  -------\t" + l + "\t sizeof = " + sizeof(long) + " Bytes");
 
         // Alteracao realizada por Isabelle
         Console.WriteLine("Linhas a mais no código -- Isabelle");
